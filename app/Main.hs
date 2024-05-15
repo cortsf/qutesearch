27356acfe0695ca37f6a2bef9b3b2067c3943636
Franco Cortesi <cortesifranco3@gmail.com>
2024-05-15 16:21:58 -0300

    Call qutesearch without <new_engine>.

--- app/Main.hs
+++ app/Main.hs
@@ -26,10 +26,26 @@ main = do
   args <- getArgs
   case args of
     [config, old_url, new_engine] -> qutesearch config old_url new_engine
+    [config, old_url] -> qutechange config old_url
     _ -> putStrLn "Wrong number of arguments"
 
+-- | qutechange: Get clean search string without changing search engine.
+qutechange :: String -> String -> IO ()
+qutechange config old_url = do
+  file <- readFile config
+  case parseEngines file of
+    Left x -> print x
+    Right engines -> do
+      putStrLn $ "engines: " <> show engines
+      let all_engines = mconcat engines
+          (_engine, url_start, url_end) = head $ sortBy (\(_, _, a) (_, _, b) -> if length a > length b then LT else GT) (filter (\(engine, url_start, url_end) -> isPrefixOf url_start old_url && isSuffixOf url_end old_url) all_engines)
+          url_minus_start = T.replace (T.pack url_start) "" (T.pack old_url)
+          search_string = if null url_end then url_minus_start else T.replace (T.pack url_end) "" url_minus_start
+      putStrLn _engine
+      putStrLn $ T.unpack search_string
 
--- | sortBy is used to disambiguate multiple matches. Otherwise the (very common) empty postfix string 
+-- | qutesearch: Switch search engine without changing search string.
+-- NOTE: sortBy is used to disambiguate multiple matches. Otherwise the (very common) empty postfix string 
 --  always matches (and gets selected) when we are actually looking for a longer postfix string.
 -- Example: 
 -- c.url.searchengines["gl"] =  "https://www.google.com/search?q={}"
@@ -51,6 +67,7 @@ qutesearch config old_url new_engine = do
           new_url = new_url_start <> T.unpack search_string <> new_url_end
       putStrLn new_url
 
+-- | [[(engine, url_start, url_end)]]
 parseEngines :: String -> Either ParseError [[(String, String, String)]]
 parseEngines = parse (manyTill (try (do
                                       string "c.url.searchengines[\""
@@ -61,3 +78,7 @@ parseEngines = parse (manyTill (try (do
                                       url_end <- manyTill anyChar $ char '"'
                                       pure [(engine, url_start, url_end)]
                                   ) <|> (manyTill anyChar newline >> pure [])) eof) "(unknown)"
+
+
+
+
